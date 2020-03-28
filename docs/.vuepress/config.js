17b205f2b9fce835483a56346acac22932e728ef
Aster <galaster@foxmail.com>
2020-03-28 23:53:21 +0800

    🚀 Update vuepress to 1.4.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,61 @@
+const locale_cn = {
+    selectText: '选择语言',
+    label: '简体中文',
+    editLinkText: '在 GitHub 上编辑此页',
+    serviceWorker: {
+        updatePopup: {
+            message: "发现新内容可用.",
+            buttonText: "刷新"
+        }
+    },
+    sidebar: {
+        "/cn/appendix/": [
+            {
+                title: '附录',
+                collapsable: true,
+                children: [
+                    ['', '附录A'],
+                    ['other', '附录B'],
+                ]
+            },
+        ]
+    }
+}
+
+const locale_en = {
+    selectText: 'Languages',
+    label: 'English',
+    ariaLabel: 'Languages',
+    editLinkText: 'Edit this page on GitHub',
+    serviceWorker: {
+        updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh"
+        }
+    },
+    sidebar: {
+        "/en/appendix/": [
+            {
+                title: 'Appendix',
+                collapsable: true,
+                children: [
+                    ['', 'Appendix A'],
+                ]
+            },
+        ]
+    }
+}
+
 module.exports = {
     dest: 'docs/.build',
     locales: {
-        '/': {
+        '/cn/': {
             lang: 'zh-CN',
+            title: 'TemplateVuepress',
+            lastUpdated: 'Last Updated',
+        },
+        '/en/': {
+            lang: 'en-US',
             title: 'TemplateVuepress',
             lastUpdated: 'Last Updated',
         }
@@ -17,15 +70,10 @@ module.exports = {
         markdown: {
             lineNumbers: true
         },
-        sidebar: [
-            {
-                title: '附录',
-                collapsable: true,
-                children: [
-                    ['/appendix', 'Appendix'],
-                ]
-            },
-        ]
+        locales: {
+            '/cn/': locale_cn,
+            '/en/': locale_en,
+        },
     },
     serviceWorker: true,
     markdown: {
